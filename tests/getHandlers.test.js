9c4b2691d1e7432b3616d1c12061b6090958fbf5
Tsvetelina (Lina) Stefanova <147356541+Lina944@users.noreply.github.com>
2024-06-04 02:49:12 +0000

    test update

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,29 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Should return 200 status code', async () => {
+	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200);
+});
+
+
+// eslint-disable-next-line no-undef
+const config = require('../config');
+
+test('Body should contain', async () => {
+    let actualResponseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
+		actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+
+	expect(actualResponseBody.name).toBe("For picnic");
 });
