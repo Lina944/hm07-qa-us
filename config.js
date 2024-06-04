ddf5e823b4223db6b7d38890a9539f2ca7af00bb
Tsvetelina (Lina) Stefanova <147356541+Lina944@users.noreply.github.com>
2024-06-04 03:11:18 +0000

    additional changes done on code

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-522cdeda-c3bb-4aa7-bef1-e56f6fbfa741.containerhub.tripleten-services.com"
 }
