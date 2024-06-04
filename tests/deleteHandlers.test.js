1d81d714bdc5c1aefffb2920a6474095a9c49b9e
Tsvetelina (Lina) Stefanova <147356541+Lina944@users.noreply.github.com>
2024-06-04 02:43:42 +0000

    updated ot test

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,12 +1,30 @@
 // eslint-disable-next-line no-undef
+// eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('status code should be 200', async () => {
+	let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
 			method: 'DELETE',
 		});
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200);
+});
+
+
+test('response body should contain confirmation that the kit has been deleted', async () => {
+	let actualResponseBody;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
+			method: 'DELETE',
+		});
+		actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualResponseBody.ok).toBe(true);
 });
